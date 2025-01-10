74d2335cbbefc32003d5f0f8d62016f81c729b1f
Ivan Daniel <84136621+doctoxer@users.noreply.github.com>
2025-01-10 12:12:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2338,7 +2338,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "sticker":
         btn = [[
             InlineKeyboardButton("⟸ Bᴀᴄᴋ", callback_data="help"),
-            InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="telegram.me/LegPieceOff')
+            InlineKeyboardButton("Cᴏɴᴛᴀᴄᴛ", url="telegram.me/LegPieceOff")
         ]]
         await client.edit_message_media(
             query.message.chat.id, 
